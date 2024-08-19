838022cc72a072eb00cc2e9704bf16fd225e2fb2
Vitor <vitor.sousa.franca@gmail.com>
2024-08-19 15:36:43 -0300

    add: machine_code folder + instruction_memory testbench + fix: ram -> memory in instruction memory

--- src/modules/instruction_memory.v
+++ src/modules/instruction_memory.v
@@ -7,19 +7,19 @@ module instruction_memory #(
     output wire [INSTRUCTION_WIDTH-1:0] instruction // Saída da instrução (10 bits)
 );
 
-    // Declaração da memória RAM como um array bidimensional
-    // A RAM tem 64 posições, cada uma com largura de 10 bits (2^6 = 64 endereços)
-    reg [INSTRUCTION_WIDTH-1:0] ram [(2**ADDR_BITS)-1:0];
+    // Declaração da memória como um array bidimensional
+    // A memória tem 64 posições, cada uma com largura de 10 bits (2^6 = 64 endereços)
+    reg [INSTRUCTION_WIDTH-1:0] memory [(2**ADDR_BITS)-1:0];
 
-    // Bloco inicial para carregar a memória RAM a partir de um arquivo de texto
+    // Bloco inicial para carregar as intruções na memória a partir de um arquivo de texto
     initial begin
-        // Lê o conteúdo do arquivo "machine_code.txt" e armazena nas posições de RAM
+        // Lê o conteúdo do arquivo "machine_code.txt" e armazena
         // O arquivo deve conter instruções em formato binário, uma por linha
-        $readmemb("machine_code.txt", ram);
+        $readmemb("machine_code/machine_code.txt", memory);
     end
 
-    // Atribuição contínua para retornar a instrução armazenada na RAM no endereço especificado
-    assign instruction = ram[address];
+    // Atribuição contínua para retornar a instrução armazenada na no endereço especificado
+    assign instruction = memory[address];
 
 endmodule
 

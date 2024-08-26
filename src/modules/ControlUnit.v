add0d04aaad43bde01f5c1a500ee12b53df306be
Adriell <adriell.pinheiro@cear.ufpb.br>
2024-08-26 11:37:48 -0300

    Todas as funções testadas

--- src/modules/ControlUnit.v
+++ src/modules/ControlUnit.v
@@ -28,25 +28,80 @@ module ControlUnit(
 
         // Decodifica a instrução
         case (opcode)
-            4'b0000: {ALU_opcode, addr_a, addr_b, load, mb_select} <= {4'b0000, instruction[5:3], instruction[2:0], 1'b1, 1'b1};
-            4'b0001: {ALU_opcode, addr_a, addr_b, load, mb_select} <= {4'b0001, instruction[5:3], instruction[2:0], 1'b1, 1'b1};
-            4'b0010: {ALU_opcode, addr_a, addr_b, load} <= {4'b0000, instruction[5:3], instruction[2:0], 1'b1}; // ADDI
-            4'b0011: {ALU_opcode, addr_a, addr_b, load} <= {4'b0001, instruction[5:3], instruction[2:0], 1'b1}; // SUBI
-            4'b0100: {ALU_opcode, addr_a, load} <= {4'b0010, instruction[5:3], 1'b1}; // MUL2
-            4'b0101: {ALU_opcode, addr_a, load} <= {4'b0011, instruction[5:3], 1'b1}; // DIV2
-            4'b0110: {addr_a, reset} <= {instruction[5:3], 1'b1}; // CLR
-            4'b0111: reset_all <= 1'b1; // RST
-            4'b1000: {ALU_opcode, addr_a, addr_b, load, mb_select} <= {4'b0100, instruction[5:3], instruction[2:0], 1'b1, 1'b1}; // MOV
+            4'b0000: begin // Operação para o opcode 0000
+                ALU_opcode <= 4'b0000;
+                addr_a <= instruction[5:3];
+                addr_b <= instruction[2:0];
+                load <= 1'b1;
+                mb_select <= 1'b1;
+            end
+            4'b0001: begin // Operação para o opcode 0001
+                ALU_opcode <= 4'b0001;
+                addr_a <= instruction[5:3];
+                addr_b <= instruction[2:0];
+                load <= 1'b1;
+                mb_select <= 1'b1;
+            end
+            4'b0010: begin // ADDI
+                ALU_opcode <= 4'b0000;
+                addr_a <= instruction[5:3];
+                addr_b <= instruction[2:0];
+                load <= 1'b1;
+            end
+            4'b0011: begin // SUBI
+                ALU_opcode <= 4'b0001;
+                addr_a <= instruction[5:3];
+                addr_b <= instruction[2:0];
+                load <= 1'b1;
+            end
+            4'b0100: begin // MUL2
+                ALU_opcode <= 4'b0010;
+                addr_a <= instruction[5:0];
+                load <= 1'b1;
+            end
+            4'b0101: begin // DIV2
+                ALU_opcode <= 4'b0011;
+                addr_a <= instruction[5:0];
+                load <= 1'b1;
+            end
+            4'b0110: begin // CLR
+                addr_a <= instruction[5:0];
+                reset <= 1'b1;
+            end
+            4'b0111: begin // RST
+                reset_all <= 1'b1;
+            end
+            4'b1000: begin // MOV
+                ALU_opcode <= 4'b0100;
+                addr_a <= instruction[5:3];
+                addr_b <= instruction[2:0];
+                load <= 1'b1;
+                mb_select <= 1'b1;
+            end
             4'b1001: begin // JMP instruction
-                        load_PC <= 1'b1;
-                        pc_value <= instruction[5:0];
-                    end 
-            4'b1010: {mem_addr, mem_read} <= {instruction[5:0], 1'b1}; // OUT
-            4'b1011: {addr_a, mem_addr, mem_read, mem_select, load} <= {3'b0, instruction[5:0], 1'b1, 1'b1, 1'b1}; // LOAD
-            4'b1100: {addr_a, mem_addr, mem_write} <= {3'b0, instruction[5:0], 1'b1}; // STORE
+                load_PC <= 1'b1;
+                pc_value <= instruction[5:0];
+            end
+            4'b1010: begin // OUT
+                mem_addr <= instruction[5:0];
+                mem_read <= 1'b1;
+            end
+            4'b1011: begin // LOAD
+                addr_a <= 3'b001;
+                mem_addr <= instruction[5:0];
+                mem_read <= 1'b1;
+                mem_select <= 1'b1;
+                load <= 1'b1;
+            end
+            4'b1100: begin // STORE
+                addr_a <= 3'b000;
+                mem_addr <= instruction[5:0];
+                mem_write <= 1'b1;
+            end
             default: begin
                 // Mantenha os valores padrão se a instrução for inválida
             end
         endcase
+
     end
 endmodule
